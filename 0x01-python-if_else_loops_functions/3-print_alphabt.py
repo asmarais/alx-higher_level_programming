8315aa21200c81ff107bf0468bcb321b665e3a7b
asmarais <rais.asma99@gmail.com>
2023-06-06 01:25:13 -0700

    project 1 python

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 for alphabet in range(ord('a'), ord('z') + 1):
+    if alphabet == ord('e') or alphabet == ord('q'):
+        continue
     letter = chr(alphabet)
     print(letter, end='')
