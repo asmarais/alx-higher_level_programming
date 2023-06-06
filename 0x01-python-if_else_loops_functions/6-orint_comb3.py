2ead9cfc83fd43a4a335aadeba3440c0a4076ec1
asmarais <94301506+asmarais@users.noreply.github.com>
2023-06-06 10:57:11 +0100

    Update 6-orint_comb3.py

--- 0x01-python-if_else_loops_functions/6-orint_comb3.py
+++ 0x01-python-if_else_loops_functions/6-orint_comb3.py
@@ -3,5 +3,5 @@ for i in range(8):
     for j in range(10):
         if i > j or i == j:
             continue
-        print(f"{i:d}{j:d}", end=', ')
-print(f"{89}")
+        print("{}{}".format(i,j), end=', ')
+print("89")
