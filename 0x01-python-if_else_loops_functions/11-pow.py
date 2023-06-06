cf42c6af451e17f4da2b1199cf2b19b0ca27bf07
asmarais <94301506+asmarais@users.noreply.github.com>
2023-06-06 11:51:34 +0100

    Update 11-pow.py

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -2,8 +2,8 @@
 def pow(a, b):
     p = 1
     if b < 0:
-    for i in range(-b):
-        p = p * 1 / a
+        for i in range(-b):
+            p = p * 1 / a
     for i in range(b):
         p = p * a
     return(p)
