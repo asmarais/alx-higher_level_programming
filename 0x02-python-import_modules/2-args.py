47bf34e2f8d5896357ce769a80596bc31da48aba
asmarais <rais.asma99@gmail.com>
2023-06-08 04:22:48 -0700

    import & modules

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
         print("1: {}".format(arguments[1]))
     else:
         if (len(arguments) == 1):
-            print("0 arguments .")
+            print("0 arguments.")
         else:
             print("{} arguments:".format(len(arguments) - 1))
             for index in range(1, len(arguments)):
