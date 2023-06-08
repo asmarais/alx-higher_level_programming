e883833cc0667202a83c6152e41967c67244aed3
asmarais <rais.asma99@gmail.com>
2023-06-08 04:18:03 -0700

    import & modules

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import sys
+arguments = sys.argv
+if __name__ == "__main__":
+    if (len(arguments) == 2):
+        print("1 argument:")
+        print("1: {}".format(arguments[1]))
+    else:
+        if (len(arguments) == 1):
+            print("0 arguments .")
+        else:
+            print("{} arguments:".format(len(arguments) - 1))
+            for index in range(1, len(arguments)):
+                print("{}: {}".format(index, arguments[index]))
