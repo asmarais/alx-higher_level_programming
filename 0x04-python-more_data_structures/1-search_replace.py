720e620f92a92fcd4398c2fd2c5b4a0787f02dfc
asmarais <94301506+asmarais@users.noreply.github.com>
2023-06-14 13:03:03 +0100

    more data structure

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -4,5 +4,6 @@ def search_replace(my_list, search, replace):
     for element in my_list:
         if element == search:
             new_list.append(replace)
-        new_list.append(element)
+        else
+            new_list.append(element)
     return new_list
