d4e8d50db3be958e3d2ddc6399a925e797278b60
asmarais <rais.asma99@gmail.com>
2023-06-27 13:59:00 -0700

    project 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''3-square.py: Defines size as an int and also >= 0'''
+
+
+class Square:
+    ''' create a classe called square'''
+
+    def __init__(self, size=0):
+        ''' initializes a class'''
+        self.__size = size
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+
+    def area(self):
+        '''returns the current square area'''
+        return self.__size * self.__size
