fdf72fd1f85b655f42ee51505d797b41181d9fad
asmarais <rais.asma99@gmail.com>
2023-06-27 13:52:50 -0700

    project 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -7,7 +7,7 @@ class Square:
 
     def __init__(self, size=0):
         ''' initializes a class'''
-        self.__size = 0
+        self.__size = size
         if type(size) is not int:
             raise TypeError('size must be an integer')
         if size < 0:
