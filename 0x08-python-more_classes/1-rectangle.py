b3351af576fd15878d1c44bf1a9e83fb9cdcd091
asmarais <94301506+asmarais@users.noreply.github.com>
2023-07-03 13:10:17 +0100

    project 1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 '''1-rectangle.py: that defines a rectangle'''
 
+
 class Rectangle:
     '''Defines the Rectangle type'''
-    
+
     def __init__(self, width=0, height=0):
         '''Initialize a class Rectangle'''
 
