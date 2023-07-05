8f069ca59f416c30b06f837a189e2bd299f84797
asmarais <rais.asma99@gmail.com>
2023-07-05 06:22:24 -0700

    correction

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,20 +1,21 @@
 def text_indentation(text):
     if not isinstance(text, str):
         raise TypeError("text must be a string")
-    
+
     separators = ['.', '?', ':']
     lines = []
     line = ''
-    
+
     for char in text:
         line += char
         if char in separators:
             lines.append(line.strip())
             line = ''
-    
+
     if line:
         lines.append(line.strip())
-    
-    for line in lines:
-        print(line)
-        print()
+
+    for i in range(len(lines)):
+        print(lines[i])
+        if i == len(lines)-1:
+            print(lines[i], end="")
