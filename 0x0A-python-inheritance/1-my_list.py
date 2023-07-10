ff07dcd04c5c06cdce74d8d81ccb26ac3c24b104
asmarais <rais.asma99@gmail.com>
2023-07-10 04:55:06 -0700

    inheritance

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''
+a class MyList that inherits from list
+'''
+class MyList(list):
+    '''print the elements of the list in an ascending sort'''
+    def print_sorted(self):
+        new = sorted(self)
+        print(new)
