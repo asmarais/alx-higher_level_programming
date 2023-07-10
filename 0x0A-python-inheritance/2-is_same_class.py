50559fce8cf55ccaf874b841f060c6b0a5997d16
asmarais <94301506+asmarais@users.noreply.github.com>
2023-07-10 13:06:07 +0100

    Create 2-is_same_class.py

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''
+2-is_same_class.py: 
+a function that returns True if the object is exactly an instance of the specified class ; otherwise False
+'''
+
+
+def is_same_class(obj, a_class):
+    '''
+    check if instance is same
+    '''
+    return issubclass(a_class, type(obj))
