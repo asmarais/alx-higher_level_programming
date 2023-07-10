e410fc3862fc87d0b7092895a94ad6991fcdcec9
asmarais <rais.asma99@gmail.com>
2023-07-10 05:46:46 -0700

    inheritance

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -6,6 +6,6 @@
 
 
 def inherits_from(obj, a_class):
-    if type(obj) == a_class:
-        return False
-    return issubclass(type(obj), a_class)
+    if type(obj) != a_class and issubclass(type(obj), a_class):
+        return True
+    return False
