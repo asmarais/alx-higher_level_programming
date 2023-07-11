84c0c4cbcdf9ad3189164e0effcf44fb3d4b2476
asmarais <rais.asma99@gmail.com>
2023-07-11 09:23:07 -0700

    project 0+1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -6,5 +6,6 @@ the number of characters written:
 
 
 def write_file(filename="", text=""):
+    '''write the number of characters'''
     with open(filename, 'w', encoding="utf-8") as f:
         return f.write(text)
