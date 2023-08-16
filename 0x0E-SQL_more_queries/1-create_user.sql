6124b5c512e823d71024c5b0f5f3016cd306e474
asmarais <94301506+asmarais@users.noreply.github.com>
2023-08-16 17:04:22 +0100

    Update 1-create_user.sql

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- Create a new user for MySQL and grant all privileges
-CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY user_0d_1_pwd;
+CREATE USER user_0d_1@localhost IDENTIFIED BY user_0d_1_pwd;
 GRANT ALL PRIVILEGES ON *.* TO user_0d_1@localhost WITH GRANT OPTION;
