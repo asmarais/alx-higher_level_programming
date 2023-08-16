45bcd5a2e67a265b82a854e29f307d073633e138
asmarais <94301506+asmarais@users.noreply.github.com>
2023-08-16 18:07:17 +0100

    Update 7-cities.sql

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,7 +1,7 @@
 -- cretae DATABASE hbtn_0d_usa and TABLE cities
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
-CREATE TABLE IF OT EXISTS cities (
+CREATE TABLE IF NOT EXISTS cities (
           id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
           state_id INT NOT NULL,
           FOREIGN KEY(state_id) REFERENCES states(id),
