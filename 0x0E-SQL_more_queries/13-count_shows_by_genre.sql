7cc4323947be6c58de06da5cc90b15d56567933b
asmarais <94301506+asmarais@users.noreply.github.com>
2023-08-16 18:56:34 +0100

    Update 13-count_shows_by_genre.sql

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,5 @@
 --
-SELECT name as 'genre', COUNT(*) AS 'number_of_shows'
+SELECT name as 'genre', COUNT(*) AS 'number_shows'
 FROM tv_genres, tv_show_genres
 WHERE tv_genres.id = tv_show_genres.genre_id
 GROUP BY tv_genres.name
