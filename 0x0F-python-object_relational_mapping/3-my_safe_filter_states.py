2156a0d632190a5fcdb95edc748eebd861091198
asmarais <94301506+asmarais@users.noreply.github.com>
2023-09-16 20:38:01 +0100

    Update 3-my_safe_filter_states.py

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -14,7 +14,7 @@ if __name__ == '__main__':
                          passwd=argv[2], db=argv[3])
 
     cur = db.cursor()
-    req = SELECT * FROM states WHERE BINARY name = %s", [argv[4]]
+    req = "SELECT * FROM states WHERE BINARY name = %s", [argv[4]]
     cur.execute(req)
 
     rows = cur.fetchall()
