acede1f7d946354105355671856384f001733e16
asmarais <94301506+asmarais@users.noreply.github.com>
2023-09-17 12:54:22 +0100

    Update 4-cities_by_state.py

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,15 +1,11 @@
-             #!/usr/bin/python3
+#!/usr/bin/python3
 """
 a script that lists all cities from the
 database hbtn_0e_4_usa
 """
 import MySQLdb
 from sys import argv
-
-# The code should not be executed when imported
 if __name__ == '__main__':
-
-    # make a connection to the database
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3])
 
