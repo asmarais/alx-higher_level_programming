1c09d2629e830110c77c0a554c25835a778a8200
asmarais <rais.asma99@gmail.com>
2023-09-28 16:13:36 +0100

    technical question

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""module that finds a peak in a list"""
+
+
+def find_peak(my_list):
+    """a function to find the peak of a list"""
+
+    if my_list is None or len(my_list) == 0:
+        return None
+    
+    if len(my_list) == 1:
+        return my_list[0]
+        
+    start = 0
+    end = len(my_list) - 1
+    while start < end:
+        mid = start + (end - start) // 2
+        if my_list[mid] > my_list[mid - 1] and my_list[mid] > my_list[mid + 1]:
+            return my_list[mid]
+        if my_list[mid - 1] > my_list[mid + 1]:
+            end = mid
+        else:
+            start = mid + 1
+    return my_list[start]
+            
+        
+    
